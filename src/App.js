c823688b37464bc7b319dae76bc548a154293e34
Danilo <methodic4u@gmail.com>
2020-02-11 14:59:13 -0300

    Passing method references between components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
-import person from './Person/Person';
 
 class App extends Component {
   state = {
@@ -12,12 +11,12 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (name) => {
     // console.log('was clicked!!')
     // DON'T DO THIS: this.state.persons[0].name = 'Vieira';
     this.setState({
       persons: [
-        {name: 'Dan', age: 37},
+        {name: name, age: 37},
         {name: 'Amanda', age: 32},
         {name: 'Valentina', age: 2}
       ]
@@ -30,12 +29,24 @@ class App extends Component {
 
         <h1>This is a react app</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
+        <button 
+          // Most used way to pass a mathod receiving a parameter
+          onClick={this.switchNameHandler.bind(this, 'Danilo!!')}>
+          Switch name
+        </button> 
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}/>
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age} 
+          // An alternative way of passing a method receiveing a parameter
+          click={() => this.switchNameHandler('Dan!!')}>
           My Robbies: handcrafting
         </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}/>
         
       </div>
     );
