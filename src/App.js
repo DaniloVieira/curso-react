17203b8adc0a7e749cc73c625dae63445f262cfe
Danilo <methodic4u@gmail.com>
2020-02-13 15:06:21 -0300

    reventing error boundary

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import classes from './App.css';
 import Person from './Person/Person';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
+//import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -56,13 +56,12 @@ class App extends Component {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <ErrorBoundary key={person.id}> 
-                      <Person
-                        change={(event) => this.nameChangeHandler(event, person.id)}
-                        click={() => this.deletePersonHandler(index)}
-                        name={person.name} 
-                        age={person.age}/>
-                    </ErrorBoundary>
+            return <Person
+                      key={person.id}
+                      change={(event) => this.nameChangeHandler(event, person.id)}
+                      click={() => this.deletePersonHandler(index)}
+                      name={person.name}
+                      age={person.age}/>
           })}
         </div>
       )
