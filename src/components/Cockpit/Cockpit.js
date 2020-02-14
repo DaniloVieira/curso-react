21c4a6a743b022eadeabf07f1223669b78d8ebd3
Danilo <methodic4u@gmail.com>
2020-02-14 14:04:37 -0300

    Cleanup Work with useEffect() - Example

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -19,10 +19,11 @@ const cockpit = (props) => {
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
         // Http request...
-        setTimeout(() => {
+        const timer = setTimeout(() => {
             alert('save data to cloud!!!');
         }, 1000);
         return () => {
+            clearTimeout(timer);
             console.log('[Cockpit.js] clenaup work in useEffect');
         }
     }, [props.persons]);
